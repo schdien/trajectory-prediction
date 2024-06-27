4c476392f319ccff9837face3090cbcc157d0993
schdien <1446417983@qq.com>
2024-06-27 12:34:08 +0800

    refactor: 修改查找轨迹结果的格式

--- src/models.py
+++ src/models.py
@@ -8,13 +8,12 @@ import matplotlib.pyplot as plt
 
 class TrajPredictor:
     def __init__(self,traj_list):
-        self.trajs = np.row_stack(traj_list)
+        self.trajs = np.vstack(traj_list)
         self.identifiers = np.concatenate([i * np.ones(len(traj),dtype=int) for i,traj in enumerate(traj_list)])
         self.edges = np.cumsum(np.array([len(traj) for traj in traj_list]))
         self.normalizer = MinMaxScaler()
         normalized_trajs = self.normalizer.fit_transform(self.trajs)
         self.tree = BallTree(normalized_trajs)
-
 
     def _get_edge_index(self,ind):
         identifier = self.identifiers[ind]
@@ -33,41 +32,6 @@ class TrajPredictor:
             return pred_ind
         else:
             raise ValueError('policy must be \'remove\' or \'edge\'')
-
-    def _query_point(self,x,step,k=100):
-        x = self.normalizer.transform(x)
-        dist,ind = self.tree.query(x,k=k)
-        weights = np.exp(-(dist/0.003)**2/2)
-        weights = weights / np.max(weights)
-        pred_ind = self._get_pred_index(ind,step)
-        pred_points = self.trajs[pred_ind]
-        return pred_points, weights
-    
-    def _cluster_point(self,points,weight,eps,min_smaples):
-        cluster = DBSCAN(eps=eps, min_samples=min_smaples)
-        cluster.fit(points,sample_weight=weight)
-        label = cluster.labels_
-        cs = np.unique(label)
-        valid_cs = cs[cs!=-1]
-        if len(valid_cs)!=0:
-            cs = valid_cs
-        points = [points[np.where(label == c)] for c in cs]
-        weight = [weight[np.where(label == c)] for c in cs]
-        weights_sum = np.array([np.sum(w) for w in weight])
-        mean = np.array([np.sum(s.T * w, axis=1) / p for s, w, p in zip(points, weight, weights_sum)])
-        prob = weights_sum / np.sum(weights_sum)
-        return np.column_stack((prob, mean)),label
-    
-    def predict_step(self, x):
-        points,weights = self._query_point(x,1)
-        mean = []
-        for i,(v,w) in enumerate(zip(points[:,2:],weights)):
-            mean_v,label = self._cluster_point(v, w,1.5,5)
-            ind = i*np.ones(mean_v.shape[0])
-            mean_v = np.column_stack((ind,mean_v))
-            mean.append(mean_v)
-        return np.row_stack(mean)
-        #return Y.squeeze(),l
     
     def predict(self,x,step):
         points,weights = self._query_point(x,step,100)
@@ -94,21 +58,12 @@ class TrajPredictor:
             end_ind = self._get_edge_index(ind)
         else: 
             end_ind = self._get_pred_index(ind,step,'edge')
-        slice_ind = [np.arange(s, e) for s, e in zip((ind + 1).T, end_ind.T)]
-        pred_trajs = [self.trajs[i] for i in slice_ind]
+        slice_ind = np.hstack([np.arange(s, e) for s, e in zip((ind + 1).T, end_ind.T)])
+        pred_trajs = self.trajs.take(slice_ind)
         return pred_trajs, dist
 
-    def _cluster_trajectory(self,trajs):
-        def dtw_distance(x,y):
-            x = x[x!=-1].reshape((-1,2))
-            y = y[y!=-1].reshape((-1,2))
-            return dtw(x,y).distance
-        trajs = trajs[:,:,:2]
-        cluster = DBSCAN(eps=10,min_samples=2,metric=dtw_distance)
-        cluster.fit(trajs.reshape((trajs.shape[0],-1)))
-        return cluster.labels_
     
-    def _clusetr_trajectory2(self,trajs):
+    def _clusetr_trajectory(self,trajs):
         ref_traj = max(trajs,key=len)
         time_index = [dtw(ref_traj,traj,step_pattern='asymmetric').index2 for traj in trajs] 
 
@@ -121,7 +76,6 @@ class TrajPredictor:
             padded_pred_trajs[i,:len(pred_traj)] = pred_traj
         labels = self._cluster_trajectory(padded_pred_trajs)
         return pred_trajs,labels
-
 
 
 if __name__=='__main__':
