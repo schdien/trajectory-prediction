87c325b54299f7cbe5a4c5d8a2b4c4e65064ff37
schdien <1446417983@qq.com>
2024-07-08 18:52:34 +0800

    fix

--- src/download.py
+++ src/download.py
@@ -4,56 +4,59 @@ import time
 
 
 class TrajDownloader:
-    def __init__(self, start, end, anums, n_days=180, save_path=r"assets"):
+    def __init__(self, start, end, anums, save_path, n_days=180):
         self.start = start
         self.end = end
         self.anums = anums
         self.n_days = n_days
-        option = webdriver.ChromeOptions()
-        prefs = {
-	        'download.default_directory': save_path,  # 设置默认下载路径
-	        "profile.default_content_setting_values.automatic_downloads": 1  # 允许多文件下载
-        }
-        option.add_experimental_option("prefs", prefs)
-        self.driver = webdriver.Edge(edge_options=option)
+        self.empty_counts = 0
+        option = webdriver.EdgeOptions()
+        option.add_experimental_option("prefs", {"download.default_directory": save_path})# 设置默认下载路径
+        self.driver = webdriver.Edge(options=option)
+        #self.driver.get("https://flightadsb.variflight.com/login")
         self.today_time = int(time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d')))
 
     def download(self):
         for anum in self.anums:
-            for i in range(self.n_days):
-                t = self.today_time - 86400 * i
-                url = "https://flightadsb.variflight.com/track-data/" + anum + "/" + t.__str__() + "000"
-                self.driver.get(url)
+            self.download_anum(anum)
 
-                while True:
+    def download_anum(self,anum):
+        for i in range(self.n_days):
+            if self.empty_counts == 10:
+                self.empty_counts = 0
+                return
+            t = self.today_time - 86400 * i
+            url = "https://flightadsb.variflight.com/track-data/" + anum + "/" + t.__str__() + "000"
+            self.driver.get(url)
+
+            while True:
+                try:
+                    self.driver.find_element("xpath", "//div[text()='No Data']")
+                    self.empty_counts += 1
+                    return
+                except:
                     try:
-                        self.driver.find_element("xpath", "//div[text()='No Data']")
+                        if self.driver.find_element("xpath", "//div[@class='flight_status gray']").text == "到达":
+                            if self.start in self.driver.find_element("xpath",
+                                                    "(//span[@class='apt-name ellipsis'])[1]").text and self.end in self.driver.find_element(
+                                                    "xpath","(//span[@class='apt-name ellipsis'])[2]").text:
+                                self.driver.find_element("xpath",
+                                                    "//div[@id='app']/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[" \
+                                                    "1]/div[3]/table[1]/tbody[1]/tr[1]/td[11]/div[1]/div[1]/div[2]/a[1] ").click()
+                            else:
+                                return
                         break
                     except:
-                        try:
-                            if self.driver.find_element("xpath", "//div[@class='flight_status gray']").text == "到达":
-                                if self.start in self.driver.find_element("xpath",
-                                                       "(//span[@class='apt-name ellipsis'])[1]").text and self.end in self.driver.find_element(
-                                                        "xpath","(//span[@class='apt-name ellipsis'])[2]").text:
-                                    self.driver.find_element("xpath",
-                                                        "//div[@id='app']/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[" \
-                                                        "1]/div[3]/table[1]/tbody[1]/tr[1]/td[11]/div[1]/div[1]/div[2]/a[1] ").click()
-                                else:
-                                    return
-                            break
-                        except:
-                            pass
-                    time.sleep(0.2)
+                        pass
+                time.sleep(0.2)
+        
 
 if __name__ == '__main__':
-    SHA_CAN = ['HO1855','MF7952','MU3852','ZH5238','GJ3888','HU7332','FM9303','MU8371','3U1318','3U1318',
-               'CZ3596','CZ3596','HO7439','HO7439','MF1396','MF1396','MU5309','3U1280','3U1280','CZ3548',
-               'CZ3548','HO7415','HO7415','MF1351','MF1351','MF3504','MU5301','3U1270','CZ3534','MF1337',
-               'HO1857','MF7954','MU3854','ZH5240','3U5147','HO5422','MF3506','MU5303','HU7132','3U1264',]
-    CAN_CTU = ['3U1163','CZ3401','HO7385','MF1237','3U1195','CZ3437','CZ3437','GJ5237','GJ5237','HO7389',
+
+    CAN_CTU = ['3U1163','CZ3401','HO7385','MF1237','3U1195','CZ3437','GJ5237','HO7389',
                'MF1261','3U8734','3U8734','CZ9558','HO5863','MF5558','MU3464','CZ7008','EU2236','TV5108',
-               '3U1201','CZ3443','CZ3443','GJ5239','GJ5239','HO7391','MF1264','CA4306','TV6264','ZH4404',
-               '3U1225','CZ3471','HO7393','MF1285','CA4320','3U8732','3U8732','CZ9556','G58726','HO5861',]
+               '3U1201','CZ3443','GJ5239','HO7391','MF1264','CA4306','TV6264','ZH4404',
+               '3U1225','CZ3471','HO7393','MF1285','CA4320','3U8732','CZ9556','G58726','HO5861',]
     GJS_CTU = ['3U8890','3U8890','CZ9616','GJ3172','MF5616','CA4182','TV6260','ZH4358','CA1405','TV6219',
                'ZH1405','CA4184','CA4184','CA1421','ZH1421','CA4186','ZH4362','TV6266','ZH4360','ZH4360',
                'CA1415','ZH1415','CA4120','ZH4328','3U8884','CZ9610','G56232','HO5889','MF5610','MU3508',
@@ -62,5 +65,5 @@ if __name__ == '__main__':
                'TV6201','ZH1231','CA1289','SC5339','ZH1289','CA1289','SC5339','ZH1289','CA1209','SC5341',
                'TV6209','ZH1209','HU7237','CA1223','SC5343','ZH1223','CA1201','G58190','SC5345','ZH1201',
                'CA1201','G58190','SC5345','ZH1201','CA1225','G58141','SC5347','ZH1225','CA1229','SC5349',]
-    downloader = TrajDownloader('上海','广州',SHA_CAN)
+    downloader = TrajDownloader('广州','成都',CAN_CTU,save_path="C:\\Users\\Bai\\Documents\\VSCode\\trajectory-prediction\\assets\\CAN_CTU")
     downloader.download()
--- src/examples/pattern_recognition.py
+++ src/examples/pattern_recognition.py
@@ -1,16 +1,18 @@
-import data
-from classify import gridding
+from src import data
+from src.classify import gridding
 import numpy as np
-from classify.dataset import TrajDataset
-from classify.module import GridEncoder,SeqentialClassifier
+from src.classify.dataset import SequenceDataset
+from src.classify.module import GridEncoder,SeqentialClassifier
 from torch.utils.data.dataloader import DataLoader
+import torch
+from torch import nn
 
 raw_trajs1 = data.load_files("H:/TrajectoryPrediction/adsb/PEK-SHA/MU5122", usecols=[7, 8, 5], num=300)
 raw_trajs2 = data.load_files("H:/TrajectoryPrediction/adsb/TNA-WUH", usecols=[7, 8, 5], num=300)
 stacked_traj = np.row_stack(raw_trajs1 + raw_trajs2)[:, :2]
 trajs1 = [data.preprocess(raw_traj)[:,:2] for raw_traj in raw_trajs1]
 trajs2 = [data.preprocess(raw_traj)[:,:2] for raw_traj in raw_trajs2]
-dataset = TrajDataset([trajs1,trajs2])
+dataset = SequenceDataset([trajs1,trajs2])
 dataloader = DataLoader(dataset,shuffle=True)
 grid_info = gridding.get_grid_info_by_len(stacked_traj,(0.02,0.02))
 net = nn.Sequential(GridEncoder(grid_info, (8, 8)),
