efeb69a1d46b518450d1088393292de733c485c9
schdien <1446417983@qq.com>
2024-08-14 11:25:23 +0800

    feat: synchronous sampling

--- src/neighbors.py
+++ src/neighbors.py
@@ -1,7 +1,25 @@
 import numpy as np
 from sklearn.neighbors import KDTree
 from sklearn.preprocessing import MinMaxScaler
+from scipy import interpolate
 
+
+def synchronous_sampling(state,traj):
+    pos = state[:,:2]
+    poss = traj[:,:2]
+    a = pos[:2] - poss[:3]
+    dist = np.linalg.norm(a, axis=1)
+    close_ind = np.argsort(dist)[:2]
+    start_ind = min(close_ind)
+    a = a[start_ind]
+    b = poss[max(close_ind)]-poss[start_ind]
+    start_ind = start_ind + abs(np.dot(a,b)/np.linalg.norm(b)**2)
+    end_ind = len(traj)
+    sync_inds = np.arange(start_ind,end_ind-1)
+    inds = np.arange(0,end_ind)
+    f = interpolate.interp1d(inds, traj.T, kind='quadratic',assume_sorted=True)
+    sync_traj = f(sync_inds).T
+    return sync_traj
 
 class SequenceQuerier:
     def __init__(self,trajs) -> None:
@@ -13,8 +31,8 @@ class SequenceQuerier:
         self.tree = KDTree(normalized_trajs)
 
     def query(self, x, step, mode, k=100):
-        x = self.normalizer.transform(x)
-        dist,inds = self.tree.query(x,k=k)
+        n_x = self.normalizer.transform(x)
+        dist,inds = self.tree.query(n_x,k=k)
         ends = self.ends[inds]
 
         if mode == 'state':
@@ -28,11 +46,19 @@ class SequenceQuerier:
             inds = inds.take(uniq)
             dist = dist.take(uniq)
             #根据索引取多个相似轨迹
+            y = []
             if step == -1:
-                y = [self.trajs[ind:end] for ind,end in zip(inds,ends)]
+                for ind,end in zip(inds,ends):
+                    traj = self.trajs[ind-1:end]
+                    y.append(synchronous_sampling(x,traj))
+                #y = [self.trajs[ind:end] for ind,end in zip(inds,ends)]
             else:
-                y = [self.trajs[ind:min(ind+step,end)] for ind,end in zip(inds,ends)]
+                for ind,end in zip(inds,ends):
+                    traj = self.trajs[ind-1:min(ind+step+1,end)]
+                    y.append(synchronous_sampling(x,traj))
+                #y = [self.trajs[ind:min(ind+step,end)] for ind,end in zip(inds,ends)]
 
         weight = np.exp(-(dist/0.003)**2/2)
         weight = weight / np.max(weight)
         return y, weight
+    
